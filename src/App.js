6ad0f502688aa888cda3af290bb119dbf9bce09c
elliottrapley <elliott.rapley@outlook.com>
2024-05-03 17:31:20 +0100

    added map()

--- src/App.js
+++ src/App.js
@@ -8,43 +8,33 @@ class App extends Component { // Changed from function to Class. App extends fro
   constructor() {
     super(); // calls the underlying constructor method. 
 
-    this.state = { // React is looking for inside the constructor inside the component.
-      name: { 
-        firstName: 'Elliott', 
-        lastName: 'Rapley' 
-      },
-      company: 'ZTM'
-    } 
-  }
+    this.state = {
+      monsters: [
+        {
+          name: 'Sarah'
+        },
+        {
+          name: 'Elliott'
+        },
+        {
+          name: 'Connor'
+        }
+      ]
+    };
+  };
 
   render() { // Using a render method and calling the the JSX/HTML code below within this function.
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>
-            Hi { this.state.firstName } { this.state.lastName }, 
-            I work at { this.state.company }
-          </p>
-          <button onClick={() => {
-            this.setState(() => {  
-              return {
-                name: {
-                  firstName: 'John', 
-                  lastName: 'Smith'
-                }
-              };
-            }, () => {
-              console.log(this.state);          
-            });
-          }}>Change name</button> 
-        </header>
+        {
+          this.state.monsters.map((monsters) => { // Using a call back function 
+            return <h1>{monsters.name}</h1>;
+          })
+        }
       </div>
     );
-
-  }
-  
+  } 
 }
 
 export default App;
