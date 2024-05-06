e9ea30717d5733c7cd17ade5b25eae0a33f30441
elliottrapley <35064509+elliottrapley@users.noreply.github.com>
2024-05-06 17:15:22 +0100

    added API connectivity

--- src/App.js
+++ src/App.js
@@ -9,27 +9,40 @@ class App extends Component { // Changed from function to Class. App extends fro
     super(); // calls the underlying constructor method. 
 
     this.state = {
-      monsters: [
-        {
-          name: 'Sarah'
-        },
-        {
-          name: 'Elliott'
-        },
-        {
-          name: 'Connor'
-        }
-      ]
+      monsters: [],
     };
+  }
+
+  // lifecycle methods - the code block below will run as soon as it's "mounted" - First time it get's mounted to the DOM.  
+  // a component will only be mounted again if after it's been un-mounted.
+  // Good for API calls - to retreive data stored elsewhere. 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users') // URL for where we need to fetch the data from - using the fetch() method. 
+      .then(response => // When we get a response from the API
+        response.json()) //calling the json() method to convert the response to json format. 
+
+        // whatever got returned from response.json, then it will get passed to (users)
+      .then((users) => 
+        this.setState(() => { // Then, use setState to add the users to the state and then render. 
+          return { monsters: users }
+        },
+      () => { // then just log this in the console of the dev tools. 
+        console.log(this.state);
+        }
+      )
+    )
   };
 
   render() { // Using a render method and calling the the JSX/HTML code below within this function.
-
     return (
       <div className="App">
         {
-          this.state.monsters.map((monsters) => { // Using a call back function 
-            return <h1>{monsters.name}</h1>;
+          this.state.monsters.map((monster) => { // Using a call back function 
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
           })
         }
       </div>
