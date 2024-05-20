5993f347d1944b528435ff063c0daa57e2515d52
elliottrapley <elliott.rapley@outlook.com>
2024-05-20 14:30:31 +0100

    Created new Card component and applied CSS styling

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,8 @@
 import { Component } from "react"; // import the Component class from react
+import Card from "../card/card.component";
 
 import './card-list.styles.css'
+
 
 class CardList extends Component {
         
@@ -13,14 +15,7 @@ class CardList extends Component {
                 {monsters.map(monster => {
                     const { name, email, id } = monster;
                     return (
-                    <div className="card-container" key={ id }>
-                        <img 
-                            alt={ 'monster ${ monster.name}' } 
-                            src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                        />
-                        <h2>{ name }</h2>
-                        <p>{ email }</p>
-                    </div>
+                        <Card monster={monster}/>
                 )})}
             </div>
         );
